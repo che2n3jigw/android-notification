a205ef649a7a91f272bd8ab27d310773e59de67e
che2n3jigw <che2n3jigw@163.com>
2025-12-23 14:28:04 +0800

    feat(project): init

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        gradlePluginPortal()
+        maven {
+            url = uri("https://maven.aliyun.com/repository/gradle-plugin")
+        }
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        maven {
+            url = uri("https://maven.aliyun.com/repository/public")
+        }
+    }
+}
+rootProject.name = "Notification Demo"
+include(":app")
+ 
